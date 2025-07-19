ada58ff69564d47d81af2bc38fdd82e9652d6cb0
Carlos Daniel <Carlos@Developer.com>
2025-07-18 21:12:33 -0600

    cambios en contacto

--- src/components/pages/contacto.jsx
+++ src/components/pages/contacto.jsx
@@ -55,6 +55,7 @@ const Contacto = ({ iconsContacto,iconosImagenes,logoSeleccionado }) => {
                                 <input
                                     name="text"
                                     type="text"
+                                    placeholder="Tu nombre"
                                     required
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-slate-600 sm:text-sm/6"
                                 />
@@ -69,6 +70,7 @@ const Contacto = ({ iconsContacto,iconosImagenes,logoSeleccionado }) => {
                                 <input
                                     name="email"
                                     type="email"
+                                    placeholder="Tu correo"
                                     required
                                     autoComplete="email"
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-slate-600 sm:text-sm/6"
